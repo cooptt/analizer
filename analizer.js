37ef0b0c69df73b0fc24457f011c2a5ee73a1bd3
Felipe <felipfmg17@gmail.com>
2018-10-08 14:33:43 -0500

    testOfferADDedToTrees added

--- analizer.js
+++ analizer.js
@@ -8,6 +8,14 @@ class Analizer {
         this.users = [];
     }
 
+
+    loadCatalogue(){
+        this.addVideoGame("God of War", "god_of_war.jpg");
+        this.addVideoGame("Halo", "halo.jpg");
+        this.addVideoGame("Call of Duty", "call_of_duty.jpg");
+        this.addVideoGame("Crash Bandicoot", "crass_bandicoot.jpg");
+    }
+
     addUser() {
         this.users.push(new User(this.users.length));
     }
@@ -131,14 +139,83 @@ class Offer {
 }
 
 
-var analizer = new Analizer()
-analizer.addUser();
-analizer.addVideoGame("God of War", "god_of_war.jpg");
-analizer.addSellOffer(0,0,17);
-analizer.addBuyOffer(0,0,18)
-console.log(analizer.getUser(0))
-console.log(analizer.getVideoGame(0).sellTree.min());
-console.log(analizer.getVideoGame(0).buyTree.min());
+var basicTest = function(){
+    var analizer = new Analizer()
+    analizer.addUser();
+    analizer.addVideoGame("God of War", "god_of_war.jpg");
+    analizer.addSellOffer(0,0,17);
+    analizer.addBuyOffer(0,0,18)
+    console.log(analizer.getUser(0))
+    console.log(analizer.getVideoGame(0).sellTree.min());
+    console.log(analizer.getVideoGame(0).buyTree.min());
+}
+
+
+var testAddUsers = function() {
+    var analizer = new Analizer();
+    analizer.addUser();
+    analizer.addUser();
+    analizer.addUser();
+    analizer.addUser();
+    analizer.addUser();
+    var result = true;
+    if( analizer.users.length != 5 ){
+        result = false;
+    }
+    console.log("testAddUsers: ",result);
+}
+
+
+var testOfferAddedToUsersLists = function() {
+    var analizer = new Analizer();
+    analizer.loadCatalogue();
+    analizer.addUser();
+    analizer.addBuyOffer(0,0,17);
+    analizer.addBuyOffer(0,1,18);
+    analizer.addBuyOffer(0,2,20);
+    analizer.addSellOffer(0,2,19);
+    analizer.addSellOffer(0,3,21);
+
+    var result = true;
+
+    if( analizer.getUser(0).buyList.length !==3){
+        result = false;
+    }
+
+    if ( analizer.getUser(0).sellList.length !==2 ){
+        result = false;
+    }
+
+    console.log("testOfferAddedToUsersLists: ",result);
+
+}
+
+
+
+var testOfferAddedToTrees = function(){
+    var analizer = new Analizer();
+    analizer.loadCatalogue();
+    analizer.addUser();
+    analizer.addUser();
+    analizer.addUser();
+
+    analizer.addBuyOffer(0,0,300);
+    analizer.addBuyOffer(0,1,200);
+    analizer.addSellOffer(0,2,600);
+    analizer.addSellOffer(0,3,500);
+
+    analizer.addBuyOffer(1,0,400);
+    analizer.addBuyOffer(1,1,100);
+    analizer.addSellOffer(1,2,300);
+    analizer.addSellOffer(1,2,700);
+}
+
+
+
+testAddUsers();
+testOfferAddedToUsersLists();
+
+
 
 
 
